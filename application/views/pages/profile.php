c3f37e123f15a1f3f2a6429e6906b70e9c49f32f
Mohammed Basith <mhdbazi@gmail.com>
2017-02-16 23:10:39 +0530

    answer.php edited

--- application/views/pages/profile.php
+++ application/views/pages/profile.php
@@ -1,33 +1,65 @@
-<div class="container">
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">NAME </h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $userdata['name']?></h1>
-	</div>
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">COLLEGE </h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $userdata['college']?></h1>
-	</div>
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">RANK </h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $rank?></h1>
-	</div>
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">LEVEL </h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $userdata['level']?></h1>
-	</div>
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">Email ID </h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $userdata['emailid']?></h1>
-	</div>
-	<div class="row">
-		<h1 class="col-sm-3 col-sm-offset-2 Dagger">MOBILE NO</h1>
-		<h1 class="col-sm-7 Dagger">: <?php echo $userdata['mobileno']?></h1>
-	</div>  
-	<div class="row">
-		<?php if($userdata['status']==2) :?>
-			<div class="text-center mt40" >
-				<a href="<?php echo base_url('index.php/shadow/status')?>" class="btn btn-lg btn-success" role="button">ADMIN LOGIN</a>
-			</div>
-		<?php endif; ?>
-	</div>
+<div class="container">
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">Name </h3>
+
+		<h3 class="col-sm-6 Dagger colblue"> <?php echo $userdata['name']?></h3>
+
+	</div>
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">College </h3>
+
+		<h3 class="col-sm-3 Dagger colblue"> <?php echo $userdata['college']?></h3>
+
+	</div>
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">Rank </h3>
+
+		<h3 class="col-sm-6 Dagger colblue"> <?php echo $rank?></h3>
+
+	</div>
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">Level </h3>
+
+		<h3 class="col-sm-6 Dagger colblue"> <?php echo $userdata['level']?></h3>
+
+	</div>
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">Email ID </h3>
+
+		<h3 class="col-sm-6 Dagger colblue"> <?php echo $userdata['emailid']?></h3>
+
+	</div>
+
+	<div class="row">
+
+		<h3 class="col-sm-3 col-sm-offset-3 Dagger colwhite">Mobile No</h3>
+
+		<h3 class="col-sm-6 Dagger colblue"> <?php echo $userdata['mobileno']?></h3>
+
+	</div>  
+
+	<div class="row">
+
+		<?php if($userdata['status']==2) :?>
+
+			<div class="text-center mt40" >
+
+				<a href="<?php echo base_url('index.php/shadow/status')?>" class="btn btn-lg btn-success" role="button">ADMIN LOGIN</a>
+
+			</div>
+
+		<?php endif; ?>
+
+	</div>
+
 </div>
